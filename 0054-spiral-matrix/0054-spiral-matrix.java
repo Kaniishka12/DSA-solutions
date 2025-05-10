74190581521b73f91324fbcc3e5de5bfc26406e8
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-05-10 17:02:30 +0530

    Time: 0 ms (100%), Space: 41.8 MB (26.66%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,34 +1,38 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         ArrayList<Integer> a = new ArrayList<>();
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return a;
-        }
-
-        int sr = 0; // Start row
-        int sc = 0; // Start column
-        int er = matrix.length - 1; // End row
-        int ec = matrix[0].length - 1; // End column
+        int sr = 0;
+        int sc = 0;
+        int er = matrix.length - 1;
+        int ec = matrix[0].length - 1;
 
         while (sr <= er && sc <= ec) {
+            // Traverse top row
             for (int j = sc; j <= ec; j++) {
                 a.add(matrix[sr][j]);
             }
+
+            // Traverse right column
             for (int i = sr + 1; i <= er; i++) {
                 a.add(matrix[i][ec]);
             }
+
+            // Traverse bottom row
             for (int j = ec - 1; j >= sc; j--) {
                 if (sr == er) {
                     break;
                 }
                 a.add(matrix[er][j]);
             }
-            for (int i = er - 1; i >= sr + 1; i--) {
+
+            // Traverse left column
+            for (int i = er - 1; i > sr; i--) {
                 if (sc == ec) {
                     break;
                 }
                 a.add(matrix[i][sc]);
             }
+
             sr++;
             sc++;
             er--;

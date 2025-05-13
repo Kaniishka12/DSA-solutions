379d24184d3272757bcfc2e2fcd2e0a87544ae95
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-05-13 18:03:36 +0530

    Time: 2 ms (75.43%), Space: 42.5 MB (77.9%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+            StringBuilder sk=new StringBuilder(s);
+            while(sk.toString().contains(part)){
+                 int index=sk.indexOf(part);
+                 sk.delete(index,index+part.length());
+            }
+return sk.toString();
+    }
+}

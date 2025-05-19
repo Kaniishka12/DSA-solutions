8edc9e021621fc95e13b236b6ad8c6463574ea91
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-05-19 09:51:14 +0530

    Time: 0 ms (100%), Space: 42.4 MB (74.56%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,44 +1,28 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-            // Create a sentinal/dummy node to start
-    ListNode returnNode = new ListNode(Integer.MIN_VALUE);
-
-    // Create a copy of this node to iterate while solving the problem
-    ListNode headNode = returnNode;
-
-    // Traverse till one of the list reaches the end
-    while (list1 != null && list2 != null) {
-
-      // Compare the 2 values of lists
-      if (list1.val <= list2.val) {
-        returnNode.next = list1;
-        list1 = list1.next;
-      } else {
-        returnNode.next = list2;
-        list2 = list2.next;
-      }
-      returnNode = returnNode.next;
+            
+    ListNode rn=new ListNode(Integer.MIN_VALUE);
+    ListNode hn=rn;
+    while(list1!=null && list2!=null){
+        if(list1.val<=list2.val){
+            rn.next=list1;
+            list1=list1.next;
+        }
+        else{
+             rn.next=list2;
+            list2=list2.next;
+        }
+        rn=rn.next;
     }
-
-    // Append the remaining list
-    if (list1 == null) {
-      returnNode.next = list2;
-    } else if (list2 == null) {
-      returnNode.next = list1;
+    if(list1==null){
+        rn.next=list2;
+    }else if(list2==null){
+     rn.next=list1;
     }
+     return hn.next;
 
-    // return the next node to sentinal node
-    return headNode.next;
+    
         }
     
 }

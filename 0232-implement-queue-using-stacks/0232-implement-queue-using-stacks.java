bb98dfe194e0b9b643253b2be09b128dea6170ab
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-05-29 14:53:07 +0530

    Time: 0 ms (100%), Space: 42.2 MB (10.63%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,39 +1,34 @@
 import java.util.Stack;
 
 class MyQueue {
-    private Stack<Integer> s1; // Acts as outputStack
-    private Stack<Integer> s2; // Acts as inputStack
+   private Stack<Integer> s1;
+   private Stack<Integer> s2;
 
-    public MyQueue() {
-        s1 = new Stack<>();
-        s2 = new Stack<>();
+    public MyQueue(){
+        s1=new Stack<>();
+        s2=new Stack<>();
     }
     
     public void push(int x) {
-        // Move all elements from s1 to s2
-        while (!s1.isEmpty()) {
-            s2.push(s1.pop());
-        }
-        
-        // Push new element into s2
-        s1.push(x);
-        
-        // Move everything back to s1
-        while (!s2.isEmpty()) {
-            s1.push(s2.pop());
-        }
+       while(!s1.isEmpty()){
+        s2.push(s1.pop());
+       }
+       s1.push(x);
+       while(!s2.isEmpty()){
+       s1.push(s2.pop());
+       }
     }
     
     public int pop() {
-        return s1.pop();  // O(1)
+       return s1.pop();
     }
     
     public int peek() {
-        return s1.peek();  // O(1)
+        return s1.peek();
     }
     
     public boolean empty() {
-        return s1.isEmpty();  // O(1)
+       return s1.isEmpty();
     }
 }
 

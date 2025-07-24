f0ecbf773763cdddc752b0a8a4e9062190cebe4b
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-07-24 09:39:22 +0530

    Time: 129 ms (88.7%), Space: 41.7 MB (61.77%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int row=board.length;
-        int col=board[0].length;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(dfs(board,word,i,j,0)){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    private boolean dfs(char[][] board, String word,int i,int j,int index){
+                 int m=board.length;
+                 int n=board[0].length;
+                 for(int i=0;i<m;i++){
+                    for(int j=0;j<n;j++){
+                        if(dfs(board,word,i,j,0)){
+                            return true;
+                        }
+                    }
+                 }
+                 return false;
+     }
+     public boolean dfs(char[][] board,String word,int i,int j,int index){
         if(index==word.length()){
             return true;
         }
@@ -20,12 +20,9 @@ class Solution {
         }
         char temp=board[i][j];
         board[i][j]='#';
-       boolean found= dfs(board,word,i+1,j,index+1)||
-        dfs(board,word,i-1,j,index+1)||
-        dfs(board,word,i,j+1,index+1)||
-        dfs(board,word,i,j-1,index+1);
-        
-       board[i][j]=temp;
-       return found;
-    }
+        boolean found=dfs(board,word,i+1,j,index+1)||
+        dfs(board,word,i-1,j,index+1)||dfs(board,word,i,j+1,index+1)||dfs(board,word,i,j-1,index+1);
+        board[i][j]=temp;
+        return found;
      }
+}

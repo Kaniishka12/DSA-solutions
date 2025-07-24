9f638fffaecfbbede502546526353639e8f22d08
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-07-24 10:24:23 +0530

    Time: 4 ms (88.03%), Space: 58.6 MB (23.44%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,20 +1,25 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-     int slow = nums[0];
-    int fast = nums[0];
+        ////use hashset 
+ 
+        // Step 1: Detect cycle using Floyd's Algorithm
+        int slow = nums[0];
+        int fast = nums[0];
 
-    // Phase 1: Detect intersection point
-    do {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-    } while (slow != fast);
+        do {
+            slow = nums[slow];       // move 1 step
+            fast = nums[nums[fast]]; // move 2 steps
+        } while (slow != fast);
 
-    // Phase 2: Find the entrance to the cycle (duplicate number)
-    slow = nums[0];
-    while (slow != fast) {
-        slow = nums[slow];
-        fast = nums[fast];
-    }
- return slow;
+        // Step 2: Find the entry point of the cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow; // or fast, both are now pointing to the duplicate
     }
 }
+
+    

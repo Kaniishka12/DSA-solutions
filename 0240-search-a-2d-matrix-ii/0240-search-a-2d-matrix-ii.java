46e3636c2289e575483590e03192057084520138
Kaniishka12 <152778647+Kaniishka12@users.noreply.github.com>
2025-07-26 10:40:43 +0530

    Time: 5 ms (99.24%), Space: 46.1 MB (58.5%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+      int r=matrix.length;
+      int c=matrix[0].length;
+      int row=0;
+      int col=matrix[0].length-1;
+      while(row<r && col>=0){
+        if(matrix[row][col]==target){
+            return true;
+        }
+        else if(matrix[row][col]<target){
+            row++;
+        }else{
+            col--;
+        }
+      }
+      return false;
+    }
+}
